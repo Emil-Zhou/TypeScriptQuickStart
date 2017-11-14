a4afbaa245003c424822ee0e0a0a1f40a8acd636
ZhouYingping <zhouyp002@outlook.com>
2017-11-14 23:45:43 +0800

    let

--- HandBook/VariableDeclarations.js
+++ HandBook/VariableDeclarations.js
@@ -1,19 +1,10 @@
-function f(condition, x) {
-    if (condition) {
-        var x_1 = 100;
-        return x_1;
+function theCityThatAlwaysSleeps() {
+    var getCity;
+    if (true) {
+        var city_1 = "Seattle";
+        getCity = function () {
+            return city_1;
+        };
     }
-    return x;
-}
-f(false, 0); // returns 0
-f(true, 0); // returns 100
-function sumMatrix(matrix) {
-    var sum = 0;
-    for (var i = 0; i < matrix.length; i++) {
-        var currentRow = matrix[i];
-        for (var i_1 = 0; i_1 < currentRow.length; i_1++) {
-            sum += currentRow[i_1];
-        }
-    }
-    return sum;
+    return getCity();
 }
--- HandBook/VariableDeclarations.ts
+++ HandBook/VariableDeclarations.ts
@@ -1,23 +1,12 @@
-function f(condition, x) {
-    if (condition) {
-        let x = 100;
-        return x;
-    }
+function theCityThatAlwaysSleeps() {
+    let getCity;
 
-    return x;
-}
-
-f(false, 0); // returns 0
-f(true, 0);  // returns 100
-
-function sumMatrix(matrix: number[][]) {
-    let sum = 0;
-    for (let i = 0; i < matrix.length; i++) {
-        var currentRow = matrix[i];
-        for (let i = 0; i < currentRow.length; i++) {
-            sum += currentRow[i];
+    if (true) {
+        let city = "Seattle";
+        getCity = function() {
+            return city;
         }
     }
 
-    return sum;
+    return getCity();
 }
