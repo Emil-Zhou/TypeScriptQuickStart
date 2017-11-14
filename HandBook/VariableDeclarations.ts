2697e7bb3bc89a83f64e4a7fa636171556f87885
ZhouYingping <zhouyp002@outlook.com>
2017-11-14 23:30:30 +0800

    let uses what some call lexocal-scoping or block-scoping

---
+++ HandBook/VariableDeclarations.ts
@@ -0,0 +1,20 @@
+for (var i = 0; i < 10; i++) {
+    // capture the current state of 'i'
+    // by invoking a function with its current value
+    (function(i) {
+        setTimeout(function() { console.log(i); }, 100 * i);
+    })(i);
+}
+
+function f(input: boolean) {
+    let a = 100;
+
+    if (input) {
+        // Still okay to reference 'a'
+        let b = a + 1;
+        return b;
+    }
+
+    // Error: 'b' doesn't exist here
+    return b;
+}
