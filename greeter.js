89f4222637a734060da81d9a50640c513d97c5ac
ZhouYingping <zhouyp002@outlook.com>
2017-11-14 22:54:48 +0800

    interface sample

--- greeter.js
+++ greeter.js
@@ -1,5 +1,5 @@
 function greeter(person) {
-    return "Hello, " + person;
+    return "Hello, " + person.firstName + " " + person.lastName;
 }
-var user = "Jane User";
+var user = { firstName: "Yingping", lastName: "Zhou" };
 document.body.innerHTML = greeter(user);
