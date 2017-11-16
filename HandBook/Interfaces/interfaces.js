bacdaddc7dd7b05ed7109b5e7d8a7ed52b00e0d8
ZhouYingping <zhouyp002@outlook.com>
2017-11-16 14:43:00 +0800

    Function Type

--- HandBook/Interfaces/interfaces.js
+++ HandBook/Interfaces/interfaces.js
@@ -1,50 +1,18 @@
 // interface LabelledValue {
 //   label: string;
 // }
-// function printLabel(labelledObj: LabelledValue) {
-//   console.log(labelledObj.label);
-// }
-// let myObj = {size: 10, label: "Size 10 Object"};
-// printLabel(myObj);
-// //==============================================
-// interface SquareConfig {
-//   color?: string;
-//   width?: number;
-// }
-// function createSquare(config: SquareConfig): {color: string; area: number} {
-//   let newSquare = {color: "white", area: 100};
-//   if (config.color) {
-//     newSquare.color = config.color;
-//   }
-//   if (config.width) {
-//     newSquare.area = config.width * config.width;
-//   }
-//   return newSquare;
-// }
-// let mySquare = createSquare({color: "black"});
-// //=====================================
-// interface SquareConfig {
-//   color?: string;
-//   width?: number;
-// }
-// function createSquare(config: SquareConfig): { color: string; area: number } {
-//   let newSquare = {color: "white", area: 100};
-//   if (config.color) {
-//     // Error: Property 'clor' does not exist on type 'SquareConfig'
-//     newSquare.color = config.clor;
-//   }
-//   if (config.width) {
-//     newSquare.area = config.width * config.width;
-//   }
-//   return newSquare;
-// }
-// let mySquare = createSquare({color: "black"});
-// //==================
-// interface Point {
-//   readonly x: number;
-//   readonly y: number;
-// }
-// let p1: Point = { x: 10, y: 20 };
-// p1.x = 5;// error readonly
-var a = [1, 2, 3, 4];
-var ro = a;
+var mySearch;
+mySearch = function (source, subString) {
+    var result = source.search(subString);
+    return result > -1;
+};
+var mySearch1;
+mySearch1 = function (src, sub) {
+    var result = src.search(sub);
+    return result > -1;
+};
+var mySearch2;
+mySearch2 = function (src, sub) {
+    var result = src.search(sub);
+    return result > -1;
+};
--- HandBook/Interfaces/interfaces.ts
+++ HandBook/Interfaces/interfaces.ts
@@ -118,3 +118,28 @@
 
 // let squareOptions = { colour: "red", width: 100 };
 // let mySquare3 = createSquare(squareOptions);
+
+// ================ Function Types
+
+interface SearchFunc {
+  (source: string, subString: string): boolean;
+}
+
+let mySearch: SearchFunc;
+mySearch = function (source: string, subString: string) {
+  let result = source.search(subString);
+  return result > -1;
+}
+
+let mySearch1: SearchFunc
+mySearch1 = function (src: string, sub: string): boolean {
+  let result = src.search(sub);
+  return result > -1;
+}
+
+let mySearch2: SearchFunc;
+mySearch2 = function(src, sub) {
+    let result = src.search(sub);
+    return result > -1;
+}
+
